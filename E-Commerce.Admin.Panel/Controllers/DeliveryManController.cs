c5d0747edc85ab5ae95c83069808003ed5cda0fb
fuaduddinador@gmail.com <fuaduddinador@gmail.com>
2023-02-09 18:14:12 +0600

    just added

---
+++ E-Commerce.Admin.Panel/Controllers/DeliveryManController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Admin.Panel.Controllers
+{
+    public class DeliveryManController : Controller
+    {
+        // GET: DeliveryMan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-Commerce.Admin.Panel/Controllers/SuperAdminDashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Admin.Panel.Controllers
+{
+    public class SuperAdminDashboardController : Controller
+    {
+        // GET: SuperAdminDashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
